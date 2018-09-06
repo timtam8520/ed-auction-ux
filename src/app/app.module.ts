a135791606b2ffcfe31a4f09ef9f1a03cd22c3db
unknown <38155769+timtam8520@users.noreply.github.com>
2018-09-06 23:51:48 +1000

    feat: using routing to prevent unauthorised access to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products.list.component';
 import { ProductComponent } from './products-list/product/product.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { Routing } from './app.routes';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthenticationInterceptor } from './services/authentication/authentication.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
-    ProductComponent
+    ProductComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    Routing
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AuthenticationGuard } from './services/authentication/authentication.guard';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthenticationGuard] },
+  { path: 'login', component: LoginComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
+
+export const Routing = RouterModule.forRoot(appRoutes);
